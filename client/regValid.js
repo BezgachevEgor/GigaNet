6db05d8adba99c934887dd0d577133ec9700dcd2
Zenvior <v.d.prisyazhnuk@mail.ru>
2024-03-25 22:10:06 +1000

    Попытка убрать ошибки с регистрации

--- client/regValid.js
+++ client/regValid.js
@@ -1,18 +1,20 @@
-let spans = []
+function hideError(){
+    let spans = []
 
-spans.push(document.getElementById("name-error"))
-spans.push(document.getElementById("family-error"))
-spans.push(document.getElementById("surname-error"))
-spans.push(document.getElementById("phone-error"))
-spans.push(document.getElementById("password-error"))
-spans.push(document.getElementById("repeat-error"))
+    spans.push(document.getElementById("name-error"))
+    spans.push(document.getElementById("family-error"))
+    spans.push(document.getElementById("surname-error"))
+    spans.push(document.getElementById("phone-error"))
+    spans.push(document.getElementById("password-error"))
+    spans.push(document.getElementById("repeat-error"))
 
-for (let errorSpan of spans){
-    errorSpan.setAttribute('hidden', 'hidden')
-}
-for (let errorSpan of spans){
-    if(errorSpan.innerText !== ''){
-        errorSpan.removeAttribute('hidden')
-        break
+    for (let errorSpan of spans){
+        errorSpan.setAttribute('hidden', 'hidden')
+    }
+    for (let errorSpan of spans){
+        if(errorSpan.innerText !== ''){
+            errorSpan.removeAttribute('hidden')
+            break
+        }
     }
 }
